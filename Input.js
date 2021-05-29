053499f928a0ea07481843cb8f4ea5b85ec57db2
201702083 <chs29359685@gmail.com>
2021-05-29 16:40:52 +0900

    Get City's name

--- Input.js
+++ Input.js
@@ -1,40 +1,48 @@
 import React, {Component} from 'react';
 import {StyleSheet,View, Text, TextInput, Button,Alert } from 'react-native';
-
 export default class Input extends React.Component{
     constructor(props){
         super(props);
-
         this.state = {
             text : "",
         };
     }
-
     render(){
         return(
-            <View >
+            <View>
                 <TextInput
-                value = {this.state.text}
-                style={styles.Search}
-                onChangeText={(text) => this.setState({text})}
+                    value = {this.state.text}
+                    style={styles.Search}
+                    onChangeText={(text) => this.setState({text})}
                 />
                 <Button
+                    style = {styles.Box}
                     title="검색"
-                    onPress = {()=> alert(this.state.text)}
+                    onPress = {()=> this.goSearch()}
                 />
             </View>
         );
     }
-
+    goSearch(){
+        this.props.navigation.navigate('DETAIL', {text:this.state.text});
+    }
 }
 
 
 const styles = StyleSheet.create({
   Search:{
+//    display:'flex',
+    margin:0,
     padding:0,
     backgroundColor:'white',
     borderWidth : 4,
     borderRadius: 10,
     align : 'center'
+  },
+  Box:{
+    fontSize:100,
+    borderRadius:100,
+    margin:0,
+    padding:0
   }
 });
