435538c77ef2a83831f9458114d430d697f4a9ca
201702083 <chs29359685@gmail.com>
2021-06-02 16:26:26 +0900

    current weather -> date time weather

--- Detail.js
+++ Detail.js
@@ -5,23 +5,32 @@ export default class DetailScreen extends Component {
     constructor(props){
         super(props);
         this.state = {
-            main : "",
+            main : null,
+            dt : null,
+            temp_current : null,
+            temp_feel : null,
             temp_min : null,
             temp_max : null,
+            clouds : null,
             hum : null,
             test:false,
         }
     }
 
     componentMount(){
-        return fetch("http://api.openweathermap.org/data/2.5/weather?q="+this.props.route.params.text+"&appid=e5ea91bc97a4a071bfb4d4d27fe05ae1")
+        return fetch("https://api.openweathermap.org/data/2.5/find?q="+ this.props.route.params.text+"&appid=e5ea91bc97a4a071bfb4d4d27fe05ae1")
         .then( (response) => response.json() )
         .then( (responseJson) => {
+        console.log(responseJson)
             this.setState({
-                main : responseJson.weather[0].main,
-                temp_min: responseJson.main.temp_min,
-                temp_max: responseJson.main.temp_max,
-                hum : responseJson.main.humidity,
+//                main : responseJson.weather[0].main,
+                dt : responseJson.list[0].dt ,
+//                temp_current : responseJson.main.temp,
+//                temp_feel : responseJson.main.feels_like,
+//                temp_min: responseJson.main.temp_min,
+//                temp_max: responseJson.main.temp_max,
+//                clouds : responseJson.clouds.all,
+//                hum : responseJson.main.humidity,
                 test : true,
             });
             console.log(this.state);
@@ -32,18 +41,19 @@ export default class DetailScreen extends Component {
     }
 
   render() {
-        return (
+            if(!this.state.test){
+                        this.componentMount();
+            }
+            return (
+                      <View>
+                        <Text style={{fontSize:30}}>How is the Weather today</Text>
+                        <Text>검색한 도시</Text>
+                        <Text style={{fontSize:30, backgroundColor: '#aaaaaa', borderRadius: 5, padding:5, margin:5}}>
+                        {this.props.route.params.text}
+                        </Text>
+                      </View>
+                    );
 
-              <View>
-
-
-                <Text style={{fontSize:30}}>How is the Weather today</Text>
-                <Text>검색한 도시</Text>
-                <Text style={{fontSize:30, backgroundColor: '#aaaaaa', borderRadius: 5, padding:5, margin:5}}>
-                {this.props.route.params.text}
-                </Text>
-              </View>
-            );
     }
 
 
